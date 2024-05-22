e2640d9828380033706d21323954d5a959ef7ac0
Vladimir <70798867+vladimirdanilin@users.noreply.github.com>
2024-05-22 11:48:05 +0300

    Query.sql Added
    SQL-запрос к вопросу 3

---
+++ Query.sql
@@ -0,0 +1,11 @@
+SELECT 
+    p.ProductName, 
+    c.CategoryName
+FROM 
+    Products p
+LEFT JOIN 
+    ProductCategories pc ON p.ProductID = pc.ProductID
+LEFT JOIN 
+    Categories c ON pc.CategoryID = c.CategoryID
+ORDER BY 
+    p.ProductName, c.CategoryName;
